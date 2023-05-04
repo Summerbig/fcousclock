e1ce6a4660921797028b52dcc744b888500239af
flying <66723819+Summerbig@users.noreply.github.com>
2023-05-05 02:29:08 +0800

    Update fcousclock.py

--- fcousclock.py
+++ fcousclock.py
@@ -1,11 +1,8 @@
+import time
 
-importtime
-import tkinter as tk
- 
-def tick():
-    # 获取当前时间
-    current_time = time.strftime('%H:%M:%S')
-    # 更新标签文本
-    clock_label.config(text=current_time)
-    # 每1秒调用一次tick函数
-    clock_label.after(1000, 
+def focus_timer(minutes):
+    print("Start focusing for {} minutes...".format(minutes))
+    time.sleep(minutes * 60)
+    print("Time's up! Take a break.")
+
+focus_timer(25) # 25分钟专注时间
